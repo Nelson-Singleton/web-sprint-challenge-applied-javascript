92792195aa9c0a272857854a103829262f340e56
nelson-singleton <nsingleton@ymail.com>
2020-08-07 12:10:17 -0500

    Prototype article creator function is now working

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,52 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const cardContainer = document.querySelector('.cards-container')
+axios.get("https://lambda-times-api.herokuapp.com/articles")
+    .then(response => {        
+        const articleList = response.data.articles
+        console.log(articleList)
+
+        function articleCreator(anArticle){
+            
+            for(let i = 0; i < anArticle.javascript.length; i++){
+            
+            const articleCard = document.createElement('div')
+            const articleHeadline = document.createElement('div')
+            const articleAuthor = document.createElement('div')
+            const articleImgContainer = document.createElement('div')
+            const articleImg = document.createElement('img')
+            const articleSpan = document.createElement('span')
+
+            articleCard.classList.add('card')
+            articleHeadline.classList.add('headline')
+            articleAuthor.classList.add('author')
+            articleImgContainer.classList.add('img-container')
+
+            cardContainer.appendChild(articleCard)
+            articleCard.appendChild(articleHeadline)
+            articleCard.appendChild(articleAuthor)
+            articleAuthor.appendChild(articleImgContainer)
+            articleAuthor.appendChild(articleSpan)
+            articleImgContainer.appendChild(articleImg)
+
+            articleHeadline.textContent = (anArticle.javascript[i].headline)
+            articleImg.src = (anArticle.javascript[i].authorPhoto)
+            articleSpan.textContent = (`By ${anArticle.javascript[i].authorName}`)
+
+            console.log(articleCard)
+
+            //dont forget to add onclick
+            } //for loop ending         
+            
+
+        }//function ending
+        console.log(articleCreator(articleList))
+
+        
+    })//.then ending
+
+    .catch(error =>{
+        debugger    
+    })
